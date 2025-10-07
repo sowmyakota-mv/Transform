15de765cd13e7ce20d48b759a11403221bbb9a22
sowmyakota-mv <mastersvisa.da31@gmail.com>
2025-10-07 11:52:38 +0530

    first commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -186,7 +186,7 @@ const Header: React.FC = () => {
       className="block md:hidden fixed left-0 right-0 z-40"
       style={{
         top: "120px",
-        height: "3px",
+        height: "4px",
         background: "linear-gradient(to right, #C4D7EF)",
         opacity: 0.4,
       }}
